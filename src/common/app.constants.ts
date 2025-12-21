c7b14bd18787ed76f4f29d7ba903a6dcb094d76e
Ivan <ipkozyrin@gmail.com>
2025-12-21 12:59:17 -0300

    fix

--- src/common/app.constants.ts
+++ src/common/app.constants.ts
@@ -2,4 +2,4 @@
  * Global constant for graceful shutdown timeout
  * Defines how long to wait for active connections to complete before forcing shutdown
  */
-export const APP_CLOSE_TIMEOUT_MS = 30000;
+export const APP_CLOSE_TIMEOUT_MS = 25000;
